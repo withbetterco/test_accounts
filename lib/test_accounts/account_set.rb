ab79cd1c590cd253c9fe85079c8f13df054193ea
Manuel Ortega <maoc84@gmail.com>
2019-10-21 09:00:55 -0700

    Adds a way to pass ids of resource objects we want to list in an account set

--- lib/test_accounts/account_set.rb
+++ lib/test_accounts/account_set.rb
@@ -2,12 +2,16 @@ module TestAccounts
   class AccountSet
     attr_accessor :resources, :attributes, :options
 
-    def initialize(resource, attributes = [], options = {})
+    def initialize(resource, resource_ids = [], attributes = [], options = {})
       @resource = resource
+      @resource_ids = resource_ids
       @attributes = attributes
       @options = options
+    end
 
-      @resources = @resource.constantize.all
+    def resources
+      @resource.constantize.all if @resource_ids.empty?
+      @resource.constantize.where(id: @resource_ids)
     end
 
   end
