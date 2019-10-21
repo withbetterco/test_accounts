afe6a57434c8da9792dfc9eaa9cd66dfdb40d62c
Manuel Ortega <maoc84@gmail.com>
2019-10-21 04:53:48 -0700

    Raises 404 not found is trying to access test accounts page in production server

--- app/controllers/test_accounts/application_controller.rb
+++ app/controllers/test_accounts/application_controller.rb
@@ -1,8 +1,19 @@
 module TestAccounts
   class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
+    before_action :raise_not_found_error, if: :production_server?
     before_action :basic_authentication
   
+    private
+
+    def production_server?
+      Rails.env.production?
+    end
+  
+    def raise_not_found_error
+      raise ActionController::RoutingError, 'Not Found'
+    end
+
     def basic_authentication
       authenticate_or_request_with_http_basic do |username, password|
         username == TestAccounts.config.basic_authentication_username && password == TestAccounts.config.basic_authentication_password
